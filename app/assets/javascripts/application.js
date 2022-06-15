b59eafe044944999b28989b4b76a882718ac9200
noxtsu <kuraraiban29@gmail.com>
2022-06-15 15:45:26 +0000

    skippr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,19 @@ $(document).ready(function(){
     $(this).css('color','red');
   });
 });
+
+
+$(document).ready(function(){
+  $("#theTarget").skippr({
+    transition : 'slide',
+    speed : 1000,
+    easing : 'easeOutQuart',
+    navType : 'block',
+    childrenElementType : 'div',
+    arrows : true,
+    autoPlay : true,
+    autoPlayDuration : 3000,
+    keyboardOnAlways : true,
+    hidePrevious : false
+  });
+});
